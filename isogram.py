ab8956b2e2b65a3a45ed628fb2be80f0f0185a2d
rosswillyoung <rosswillyoung@gmail.com>
2017-11-13 14:33:36 -0500

    Add files via upload

---
+++ isogram.py
@@ -0,0 +1,15 @@
+def is_isogram(string):
+    string = list(string.lower())
+    print(string)
+    count = []
+    for i in string:
+        if i == '-' or i == ' ':
+            continue
+        count.append(string.count(i))
+    return all(i == 1 for i in count)
+        
+            
+        
+           
+
+print(is_isogram(input('Give me a string')))
